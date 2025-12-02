f9e980312c97b1d52b42eb1c2e0960cf891d354b
ZakKnight77 <zakknight88@yahoo.com>
2025-12-02 15:44:19 +0000

    Updated JSON file loader for structure checking

--- json_file_loader.py
+++ json_file_loader.py
@@ -1,7 +1,15 @@
 import json
 
 
-def get_game_data(file_loc: str) -> dict:  # main func called to return game data dict
+def get_game_data() -> dict:  # main func called to return game data dict
+    data: dict = load_json_file("game_data.json")
+    if is_game_data_valid(data):
+        # return data only if it fits the structure
+        return data
+    return {}
+
+
+def load_json_file(file_loc: str) -> dict:
     try:
         with open(file_loc, "r") as file:
             data = json.load(file)
@@ -12,3 +20,32 @@ def get_game_data(file_loc: str) -> dict:  # main func called to return game dat
     except Exception as e:
         print(e)
         return {}
+
+
+def is_game_data_valid(game_data: dict) -> bool:  # this returns bool for if the data is valid
+    valid_room_data_keys: list[str] = ["description", "end_game", "items", "directions"]
+    valid_item_data_keys: list[str] = ["description", "collectible"]
+
+    if len(game_data) == 0:
+        return True
+    for room in game_data:
+        if not isinstance(game_data[room], dict):
+            print("Room data should be a dict")
+            return False
+        
+        for room_key in game_data[room]:
+            if not room_key in valid_room_data_keys:
+                print(f"Room key {room_key} not valid")
+                return False
+            
+        for item_val in game_data[room]["items"].values():
+            if not isinstance(item_val, dict):
+                print("Item data should be a dict")
+                return False
+            
+            for item_key in item_val:
+                if not item_key in valid_item_data_keys:
+                    print(f"Item key {item_key} is not  valid")
+                    return False
+    return True
+

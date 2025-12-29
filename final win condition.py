29035307f076f22cc99088c21da2255e760bab03
Nicoleta445 <nico.toot27@gmail.com>
2025-12-29 15:17:17 +0000

    Fixed the end-game handler to prevent crashes
    Changed the game_state.get() for the proper lookup of current room JSON data. Changed the end_game_handler to handle user input without crashing. Added return values for win or lose so that the main game loop can exit properly.

--- final win condition.py
+++ final win condition.py
@@ -1,18 +1,22 @@
 def check_if_end_game(game_state):
-    if game_state.get("end_game") is True: #if endgame in the loaded json file is true, it will return the other function
+    # if endgame in the loaded json file is true, it will return the other function
+    room = game_state.world[game_state.current_room]
+    if room.get("end_game") is True:
         return end_game_handler
-    return None #if endgame is false, it wont show anything and not triggering the end game handler
+    return None  # if endgame is false, it wont show anything and not triggering the end game handler
 
 
 def end_game_handler(): 
     print("You have reached the final moment.")
-    choice = input("Choose win or lose: ") #gives generic place holder option to win or lose
+    choice = input("Choose win or lose: ").strip().lower()  # gives generic place holder option to win or lose
 
-    losing_choices = {"lose"} #creates variable for losing choices (open for multiple if needed), setting the losing choice
+    losing_choices = {"lose"}  # creates variable for losing choices (open for multiple if needed), setting the losing choice
 
-    if choice in losing_choices: #goes through the losing choices variable to see if user input matches
+    if choice in losing_choices:  # goes through the losing choices variable to see if user input matches
         print("you lost.")
+        return "lose"
     else:
         print("You win.")
+        return "win"
 
     # Game ends here
